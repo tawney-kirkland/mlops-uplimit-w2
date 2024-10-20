d3ac1eb2011f1e1bd3e49681899b26fdcac9fb1f
Tawney <74927862+tawney-kirkland@users.noreply.github.com>
2024-10-20 16:01:25 +0000

    Add tests

--- project/scripts/query_api.py
+++ project/scripts/query_api.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 
-data = {"review": "Hello world this is the best product ever!"}
+data = {"review": "I really don't like this product that much"}
 headers = {"Content-Type": "application/json"}
 response = requests.post(
     "http://127.0.0.1:8000/predict", data=json.dumps(data), headers=headers
--- project/tests/e2e_test/test_api.py
+++ project/tests/e2e_test/test_api.py
@@ -1,6 +1,26 @@
 import requests
+import pytest
+from src.constants import SentimentLabel
+from src.data_models import SimpleModelRequest
 
+@pytest.mark.parametrize(
+        "review",
+        [
+            "This product is awesome.",
+            "I don't think I like this one - I am not sure yet.",
+            "This product stinks - waste of money.",
+            "I don't love this one but it is okay.",
+            "Absolute trash.",
+            "This is great - exactly what I needed!"
+        ],
+)
 
-# TODO: Add in test cases to verify that the model in the endpoint works as
-# excpected given a review
-def test_predict_endpoint_with_different_reviews(predict_url): ...
+def test_predict_endpoint_with_different_reviews(predict_url, review):
+    body = SimpleModelRequest(review=review).model_dump()
+    response = requests.post(predict_url, json=body)
+    response_json = response.json()
+    print(response_json)
+
+    assert response.status_code == 200
+    assert response_json["label"] in [label.value for label in SentimentLabel]
+    assert 0 <= response_json["score"] <= 1
